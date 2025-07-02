fa1544d7c57b026a42c0f9f3388b945d52124737
rea1got <vadyushkin@hotmail.com>
2025-07-03 01:05:35 +0300

    separate too complex init_cubic_grid()

--- src/volume.hpp
+++ src/volume.hpp
@@ -29,8 +29,9 @@ private:
     }
   }
 
-  void init_cubic_grid(int number_of_molecules, int seed) {
-    int n = static_cast<int>(std::ceil(std::cbrt(number_of_molecules)));
+  void init_cubic_grid() {
+    int n =
+        static_cast<int>(std::ceil(std::cbrt(space.get_amount_of_molecules())));
 
     if (n == 0)
       n = 1;
@@ -39,25 +40,16 @@ private:
     double dy = length_y / n;
     double dz = length_z / n;
 
-    std::srand(seed);
-
     int count = 0;
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < n; j++) {
         for (int k = 0; k < n; k++) {
-          if (count >= number_of_molecules)
+          if (count >= space.get_amount_of_molecules())
             break;
 
           auto &curr_mol = space.get_molecule(count);
-
           std::array<double, 3> coord = {i * dx, j * dy, k * dz};
           curr_mol.set_coordinate(coord);
-
-          std::array<double, 3> velocity = {
-              static_cast<double>(std::rand()) / RAND_MAX - 0.5,
-              static_cast<double>(std::rand()) / RAND_MAX - 0.5,
-              static_cast<double>(std::rand()) / RAND_MAX - 0.5};
-          curr_mol.set_velocity(velocity);
 
           count++;
         }
@@ -65,12 +57,23 @@ private:
     }
   }
 
+  void init_velocity(int seed) {
+    std::srand(seed);
+    for (int i = 0; i < space.get_amount_of_molecules(); i++) {
+      std::array<double, 3> velocity = {
+          static_cast<double>(std::rand()) / RAND_MAX - 0.5,
+          static_cast<double>(std::rand()) / RAND_MAX - 0.5,
+          static_cast<double>(std::rand()) / RAND_MAX - 0.5};
+      space.get_molecule(i).set_velocity(velocity);
+    }
+  }
+
 public:
   Volume(int number_of_molecules, int seed, double temperature = 1.0,
-         double MAX_VELOCITY = 1.0, double x = 1.0, double y = 1.0,
-         double z = 1.0)
+         double x = 1.0, double y = 1.0, double z = 1.0)
       : length_x(x), length_y(y), length_z(z), space(number_of_molecules) {
-    init_cubic_grid(number_of_molecules, seed);
+    init_cubic_grid();
+    init_velocity(seed);
     border_periodic();
     space.remove_total_momentum(temperature);
   }
