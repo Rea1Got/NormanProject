fix energy calculation based on approximation

src/graphic_velocity.py
@@ -2,10 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import json
 
-NUM_BARS = 30  # number of points on plot
-THERM_BALANCE = 10  # number of first sample after thermodynamic balance 
-EXCLUDE_LAST = 5  # exclude last EXCLUDE_LAST points
-BOLTZMANN = 1.38069E-23
+NUM_BARS = 40  # number of points on plot
+THERM_BALANCE = 150  # number of first sample after thermodynamic balance 
+EXCLUDE_LAST = 15  # exclude last EXCLUDE_LAST points
 
 with open("cfg/cfg.json", "r") as f:
     cfg = json.load(f)
@@ -18,25 +17,23 @@ with open("cfg/cfg.json", "r") as f:
     f.close()
 
 data_generated = np.loadtxt(file_data)
-vel_coord = [[], [], []]
+vel_coord_2 = [[], [], []]
 velocity = []
 kinetic_energy_per_step = []
 for i in range(THERM_BALANCE, len(data_generated)):
     step_energy = 0
     for j in range(0, len(data_generated[i]), 3):
         velocity.append(np.sqrt(data_generated[i][j]**2 + data_generated[i][j+1]**2 + data_generated[i][j+2]**2))
-        vel_coord[0].append(data_generated[i][j]**2)
-        vel_coord[1].append(data_generated[i][j+1]**2)
-        vel_coord[2].append(data_generated[i][j+2]**2)
-        step_energy += 0.5 * (data_generated[i][j]**2 + data_generated[i][j+1]**2 + data_generated[i][j+2]**2)
-    kinetic_energy_per_step.append(step_energy)
+        vel_coord_2[0].append(data_generated[i][j]**2)
+        vel_coord_2[1].append(data_generated[i][j+1]**2)
+        vel_coord_2[2].append(data_generated[i][j+2]**2)
 
 velocity.sort()
 for i in range(3):
-    vel_coord[i].sort()
+    vel_coord_2[i].sort()
 
 vel_max = velocity[-1]
-vel_coord_max = [vel_coord[0][-1], vel_coord[1][-1], vel_coord[2][-1]]
+vel_coord_max_2 = [vel_coord_2[0][-1], vel_coord_2[1][-1], vel_coord_2[2][-1]]
 
 prob = np.zeros(NUM_BARS)
 
@@ -45,10 +42,11 @@ for i in range(len(velocity)):
         if (j/NUM_BARS*vel_max <= velocity[i] < (j+1)/NUM_BARS*vel_max):
             prob[j] += 1/len(velocity)
 
-all_squared = np.concatenate(vel_coord)
-v2_mean = all_squared.mean()
+all_squared = np.concatenate(vel_coord_2)
+vel_np = np.array(velocity)
+v2_mean = vel_np.mean()**2
 
-total_max = max(vel_coord_max)
+total_max = max(vel_coord_max_2)
 bin_edges = np.linspace(0, total_max, NUM_BARS + 1)
 hist, _ = np.histogram(all_squared, bins=bin_edges)
 prob_avg = hist / len(all_squared)
@@ -72,38 +70,31 @@ ss_tot = np.sum((y_log - np.mean(y_log))**2)
 r_squared = 1 - (ss_res / ss_tot)
 
 ########################################################################## 
-print(f"Аппроксимация: ln(y) = {k:.6f}x + {b:.6f}")
 print(f"Экспонента: y = {np.exp(b):.6f} * exp({k:.6f}x)")
-print(f"R² = {r_squared:.6f}")
+print(f"R² = {r_squared:.6f}\n")
+
 print(f"Средний квадрат скорости = {v2_mean:.6f}")
-physical_temp = v2_mean * (epsilone_real / BOLTZMANN)
-print(f"Физическая температура = {physical_temp:.6f} K")
-avg_kinetic_energy = np.mean(kinetic_energy_per_step)
-print(f"Средняя кинетическая энергия системы: {avg_kinetic_energy:.2f} безразм. ед.")
-
-print(f"\nЦелевая постоянная Больцмана: {BOLTZMANN:.4e} Дж/К")
-k_B_corrected = - m_real / (2 * k * physical_temp)
-print(f"Постоянная Больцмана: {k_B_corrected:.6e} Дж/К")
-
+print(f"Температура через энергию = {v2_mean/3:.6f}")
+print(f"Температура через аппроксимации = {-1/(2*k):.6f}")
 # error_pct = abs(k_B_corrected - BOLTZMANN) / BOLTZMANN * 100
 # print(f"Погрешность: {error_pct:.6f}%")
 ########################################################################## 
 
-kinetic_energy_per_step = []
-for i in range(len(data_generated)):
-    step_energy = 0.5 * np.sum(data_generated[i]**2)
-    kinetic_energy_per_step.append(step_energy)
-
-plt.figure(figsize=(12, 6))
-plt.plot(kinetic_energy_per_step)
-plt.xlabel('Номер шага')
-plt.ylabel('Кинетическая энергия системы')
-plt.title('Эволюция кинетической энергии')
-# plt.legend()
-plt.grid(True)
-plt.savefig('energy_evolution.png')
-plt.show()
-
+# kinetic_energy_per_step = []
+# for i in range(len(data_generated)):
+#     step_energy = 0.5 * np.sum(data_generated[i]**2)
+#     kinetic_energy_per_step.append(step_energy)
+#
+# plt.figure(figsize=(12, 6))
+# plt.plot(kinetic_energy_per_step)
+# plt.xlabel('Номер шага')
+# plt.ylabel('Кинетическая энергия системы')
+# plt.title('Эволюция кинетической энергии')
+# # plt.legend()
+# plt.grid(True)
+# plt.savefig('energy_evolution.png')
+# plt.show()
+#
 ########################################################################## 
 fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(8, 10))
 plt.subplots_adjust(hspace=0.4)  